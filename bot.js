02d5e84f61097129dbfdfe2f7a33e32d166a47f6
audiosprite <jpfiorilla@gmail.com>
2017-01-25 18:06:50 -0500

    replies

--- bot.js
+++ bot.js
@@ -1,5 +1,8 @@
 const Twit = require('twit');
-// const T = new Twit();
+const t = require('./t');
+const T = new Twit(t);
+const stream = T.stream('user');
+
 const congress = require('./congress').objects;
 
 let query = 'tillis';
@@ -10,7 +13,11 @@ let query = 'tillis';
 
 const findOfficialByName = function(query){
     let matches = [], queryArr = query.split(' ');
-    for (var i = 0; i < congress.length; i++){
+    for (var i = 0; i < queryArr.length; i++){
+        if (queryArr[i][0] === '@') queryArr.shift();
+        else break;
+    }
+    for (i = 0; i < congress.length; i++){
         let subnames = 0;
         for (var j = 0; j < queryArr.length; j++){
             if (congress[i].person.name.toLowerCase().includes(queryArr[j].toLowerCase())) subnames++;
@@ -33,26 +40,41 @@ const testfindOfficialByName = function(){
 }
 */
 
-const reply = function(tweet){
+function tweeted(err, reply) {
+    if (err !== undefined) {
+        console.log(err);
+    } else {
+        console.log('Tweeted: ' + reply);
+    }
+};
+
+const replyWithInfo = function(tweet){
     // Who is this in reply to?
     var reply_to = tweet.in_reply_to_screen_name;
     // Who sent the tweet?
-    // var name = tweet.user.screen_name;
-    var name = 'gary';
+    var name = tweet.user.screen_name;
+    // var name = 'gary';
     // What is the text?
-    // var txt = tweet.text;
-    var txt = tweet;
-    txt = txt.replace(/@a2zitp/g,'');
+    var txt = tweet.text;
+    // var txt = tweet;
+    console.log(txt);
+    // txt = txt.replace(/@callmyrep/g,'');
+    // console.log(txt);
     var reply = '@'+name + ' ';
     let official = findOfficialByName(txt);
     if (official.length === 1){
         official = official[0];
+        official.person.twitterid ? 
+        reply += 'contact @' + official.person.twitterid + ' at ' + official.phone :
         reply += 'contact ' + official.person.name + ' at ' + official.phone;
     } else {
         reply += 'please be more specific with your query'
     }
-    return reply;
+    // return reply;
+    T.post('statuses/update', { status: reply }, tweeted);
 }
 
-let f = reply(query);
-console.log(f);
+stream.on('tweet', replyWithInfo);
+
+// let f = reply(query);
+// console.log(f);
